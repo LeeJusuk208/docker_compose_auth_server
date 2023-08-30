52dcc2f06c874a2e33391c3ef6d21fd1c5b0c117
Leejusuk208 <leejusuk208@gmail.com>
2023-08-31 00:58:34 +0900

    set container scale

--- run_docker_compose.sh
+++ run_docker_compose.sh
@@ -2,7 +2,11 @@
 # apt-get install -y jq
 
 json=$( cat docker_compose_config.json )
-auth_server_scale=$( echo $json | jq -r .auth_server_scale )
-front_server_scale=$( echo $json | jq -r .front_server_scale )
 
-docker-compose up --scale auth-server=$auth_server_scale --scale front-server=$front_server_scale
+front_server_scale=$( echo $json | jq -r .front_server_scale )
+api_server_scale=$( echo $json | jq -r .api_server_scale )
+auth_server_scale=$( echo $json | jq -r .auth_server_scale )
+goods_server_scale=$( echo $json | jq -r .goods_server_scale )
+alarm_server_scale=$( echo $json | jq -r .alarm_server_scale )
+
+docker-compose up -d --scale front-server=$front_server_scale --scale api-server=$api_server_scale --scale auth-server=$auth_server_scale --scale goods-server=$goods_server_scale --scale alarm-server=$alarm_server_scale --remove-orphans
